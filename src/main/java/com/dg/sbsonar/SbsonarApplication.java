dcd4d49203b82fd3ec27b25a6ed80b0db6d94e08
Deepak Shettigar <deepaksshettigar@gmail.com>
2022-01-14 15:08:54 +0530

    Initial Commit

---
+++ src/main/java/com/dg/sbsonar/SbsonarApplication.java
@@ -0,0 +1,13 @@
+package com.dg.sbsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dg/sbsonar/SbsonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.sbsonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
